a95d3950ce6242d2f1cd38a0c22cfc0131126299
gustavnybergs <Gustavnybergs@gmail.com>
2025-08-25 17:07:41 +0200

    feat: Lägger till GET /counter/increment med @RequestParam

--- src/main/java/com/gustav/webservice_lektion3/CounterController.java
+++ src/main/java/com/gustav/webservice_lektion3/CounterController.java
@@ -2,6 +2,7 @@ package com.gustav.webservice_lektion3;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class CounterController {
@@ -10,4 +11,11 @@ public class CounterController {
     public Counter getCounter() {
         return new Counter(1, 0); // valfria startvärden
     }
+
+
+    @GetMapping("/counter/increment")
+public Counter increment(@RequestParam(defaultValue = "1") int step) {
+    if (step < 0) throw new IllegalArgumentException("step måste vara >= 0");
+    return new Counter(1, 0 + step);
+}
 }
